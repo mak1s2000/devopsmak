49a9556e816dc9ce816b538bcc11721e54bdabf7
Seyamak Shahrzad <132718980+mak1s2000@users.noreply.github.com>
2023-05-24 13:41:05 -0700

    Create hostcheck.sh

---
+++ hostcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Please enter the hostname"
+read HOST;
+while true;
+do
+host $HOST.storage.XXXXcloud.com
+sleep 2
+
+"echo"
+
+echo "Now check for Geo Policy Replication status by running GET"
+
+break
+
+done
