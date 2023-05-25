bac130e2278bdd3e3204a44dd8afcb8742294234
Seyamak Shahrzad <132718980+mak1s2000@users.noreply.github.com>
2023-05-25 12:01:19 -0700

    Create listfunction.py

---
+++ listfunction.py
@@ -0,0 +1,8 @@
+Colors = ["Red", "Blue", "Green", "Yellow"]
+Numbers = [1, 5, 12, 34]
+#Numbers.extend(Colors)
+Numbers.append(23)
+#Numbers.remove(5)
+#Numbers.insert(1, 4)
+#Numbers.pop()
+print(Numbers)
