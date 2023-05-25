798851d4224a0eb0bc2a86e0498c20a789d24f35
Seyamak Shahrzad <132718980+mak1s2000@users.noreply.github.com>
2023-05-25 12:05:39 -0700

    Create simp_calculator.py

---
+++ simp_calculator.py
@@ -0,0 +1,14 @@
+#x = input("Enter a number: ")
+#y = input("Enter another number: ")
+#result = int(x) + int(y)
+
+#print(result)
+
+#However, the script above only calculates whole number and not decimal numbers, for decimal number and whole numbers,
+#the command, float needs to be used.
+
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+
+result = float(x) + float(y)
+print (result)
